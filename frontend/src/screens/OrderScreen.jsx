3d62f592a6554c36de155db6d38e2acb93b0def6
AntoxaHolovenko220 <antonio22022005@gmail.com>
2024-04-08 19:27:30 +0300

    PayPal Buttons

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -6,7 +6,7 @@ import Loader from '../components/Loader'
 import {
 	useGetOrderDetailsQuery,
 	usePayOrderMutation,
-	useGetPayPalClientIdMutation,
+	useGetPayPalClientIdQuery,
 } from '../slices/ordersApiSlice'
 import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js'
 import { toast } from 'react-toastify'
@@ -30,7 +30,7 @@ const OrderScreen = () => {
 		data: paypal,
 		isLoading: loadingPayPal,
 		error: errorPayPal,
-	} = useGetOrderDetailsQuery()
+	} = useGetPayPalClientIdQuery()
 
 	const { userInfo } = useSelector(state => state.auth)
 
@@ -58,7 +58,41 @@ const OrderScreen = () => {
 		}
 	}, [order, paypal, paypalDispatch, loadingPayPal, errorPayPal])
 
-	console.log(order)
+	function onApprove(data, actions) {
+		return actions.order.capture().then(async function (details) {
+			try {
+				await payOrder({ orderId, details })
+				refetch()
+				toast.success('Payment successful')
+			} catch (error) {
+				toast.error(error?.data?.message || error.message)
+			}
+		})
+	}
+
+	async function onApproveTest() {
+		await payOrder({ orderId, details: { paypal: {} } })
+		refetch()
+		toast.success('Payment successful')
+	}
+	function onError(error) {
+		toast.error(error.message)
+	}
+	function createOrder(data, actions) {
+		return actions.order
+			.create({
+				purchase_units: [
+					{
+						amount: {
+							value: order.totalPrice,
+						},
+					},
+				],
+			})
+			.then(orderId => {
+				return orderId
+			})
+	}
 
 	return isLoading ? (
 		<Loader />
@@ -152,7 +186,33 @@ const OrderScreen = () => {
 									<Col>${order.totalPrice}</Col>
 								</Row>
 							</ListGroup.Item>
-							{/* PAY ORDER PLACEHOLDER */}
+
+							{!order.isPaid && (
+								<ListGroup.Item>
+									{loadingPay && <Loader />}
+
+									{isPending ? (
+										<Loader />
+									) : (
+										<div>
+											<Button
+												onClick={onApproveTest}
+												style={{ marginBottom: '10px' }}
+											>
+												Test Pay Order
+											</Button>
+											<div>
+												<PayPalButtons
+													createOrder={createOrder}
+													onApprove={onApprove}
+													onError={onError}
+												/>
+											</div>
+										</div>
+									)}
+								</ListGroup.Item>
+							)}
+
 							{/* MARK AS DELIVERED PLACEHOLDER */}
 						</ListGroup>
 					</Card>
--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -17,13 +17,13 @@ export const orderApiSlice = apiSlice.injectEndpoints({
 			keepUnusedDataFor: 5,
 		}),
 		payOrder: builder.mutation({
-			query: (orderId, details) => ({
+			query: ({ orderId, details }) => ({
 				url: `${ORDERS_URL}/${orderId}/pay`,
 				method: 'PUT',
 				body: { ...details },
 			}),
 		}),
-		getPayPalClientId: builder.mutation({
+		getPayPalClientId: builder.query({
 			query: () => ({
 				url: PAYPAL_URL,
 			}),
@@ -36,5 +36,5 @@ export const {
 	useCreateOrderMutation,
 	useGetOrderDetailsQuery,
 	usePayOrderMutation,
-	useGetPayPalClientIdMutation,
+	useGetPayPalClientIdQuery,
 } = orderApiSlice
