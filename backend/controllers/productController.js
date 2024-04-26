eefa1241cfc95d47f0f203415824f7ef4afd5804
AntoxaHolovenko220 <antonio22022005@gmail.com>
2024-04-26 19:33:10 +0300

    Create Reviews Backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,16 +1,16 @@
 import asyncHandler from '../middleware/asyncHandler.js'
 import Product from '../models/productModel.js'
 
-// @desc		Fetch all products
-// @route		GET /api/products
+// @desc    Fetch all products
+// @route    GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
 	const products = await Product.find({})
 	res.json(products)
 })
 
-// @desc		Fetch a product
-// @route		GET /api/products/:id
+// @desc    Fetch a product
+// @route    GET /api/products/:id
 // @access  Public
 const getProductById = asyncHandler(async (req, res) => {
 	const product = await Product.findById(req.params.id)
@@ -23,8 +23,8 @@ const getProductById = asyncHandler(async (req, res) => {
 	}
 })
 
-// @desc		Create products
-// @route		POST /api/products
+// @desc    Create a products
+// @route    Post /api/products
 // @access  Private/Admin
 const createProduct = asyncHandler(async (req, res) => {
 	const product = new Product({
@@ -43,8 +43,8 @@ const createProduct = asyncHandler(async (req, res) => {
 	res.status(201).json(createdProduct)
 })
 
-// @desc		Update all products
-// @route		PUT /api/products
+// @desc    Update a product
+// @route    PUT /api/products/:id
 // @access  Private/Admin
 const updateProduct = asyncHandler(async (req, res) => {
 	const { name, price, description, image, brand, category, countInStock } =
@@ -61,8 +61,8 @@ const updateProduct = asyncHandler(async (req, res) => {
 		product.category = category
 		product.countInStock = countInStock
 
-		const updateProducts = await product.save()
-		res.json(updateProducts)
+		const updateProduct = await product.save()
+		res.json(updateProduct)
 	} else {
 		res.status(404)
 		throw new Error('Resource not found')
@@ -84,10 +84,52 @@ const deleteProduct = asyncHandler(async (req, res) => {
 	}
 })
 
+// @desc    Create a new review
+// @route    POST /api/products/:id/reviews
+// @access  Private
+const createProductReview = asyncHandler(async (req, res) => {
+	const { rating, comment } = req.body
+
+	const product = await Product.findById(req.params.id)
+
+	if (product) {
+		const alreadyReviewed = product.reviews.find(
+			review => review.user.toString() === req.user._id.toString()
+		)
+
+		if (alreadyReviewed) {
+			res.status(400)
+			throw new Error('Product already reviewed')
+		}
+
+		const review = {
+			name: req.user.name,
+			rating: Number(rating),
+			comment,
+			user: req.user._id,
+		}
+
+		product.reviews.push(review)
+
+		product.numReviews = product.reviews.length
+
+		product.rating =
+			product.reviews.reduce((acc, review) => acc + review.rating, 0) /
+			product.reviews.length
+
+		await product.save()
+		res.status(201).json({ message: 'Review added' })
+	} else {
+		res.status(404)
+		throw new Error('Resource not found')
+	}
+})
+
 export {
 	getProducts,
 	getProductById,
 	createProduct,
 	updateProduct,
 	deleteProduct,
+	createProductReview,
 }
